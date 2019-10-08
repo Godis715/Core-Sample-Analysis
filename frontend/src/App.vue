5cc8f9ca1ddafcca01aa4935a2a1b97780b5f580
unknown <godis7.15@gmail.com>
2019-10-08 21:07:17 +0300

    Started site's pages and client authentication

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    // checking, if server didn't accept the token
+    axios.interceptors.response.use(undefined, async err => {
+      if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+        this.$store.dispatch('AUTH_LOGOUT');
+        this.$router.push('/login');
+      }
+      throw err;
+    });
+
+    // append token to all headers
+    const token = localStorage.getItem('user-token');
+    if (token) {
+      axios.defaults.headers.common['Authorization'] = token;
+    }
+  }
 }
 </script>
 
@@ -16,7 +32,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ frontend/src/components/pages/Login.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <form @submit.prevent="login">
+        <h1>Sign in</h1>
+        <div>
+            <input required id="username" v-model="username" type="text" placeholder="User42" />
+            <label for="username">Login</label>
+        </div>
+
+        <div>
+            <input required id="password" v-model="password" type="password" placeholder="Password" />
+            <label for="password">Password</label>
+        </div>
+
+        <button type="submit">Login</button>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            const user = {
+                name: this.username,
+                password: this.password 
+            };
+
+            console.log('Try login:');
+            console.log('username: ' + info.name + ', password: ' + info.password);
+            
+            this.$store.dispatch('AUTH_REQUEST', user).then(() => {
+                this.$router.push('/');
+            });
+        }
+    }
+};
+</script>
