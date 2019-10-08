a17e0f9f14a2b9f0c0c3923af949cbabc326bf02
Терещенко Дмитрий <dima123-99@mail.ru>
2019-10-08 22:02:32 +0300

    Tryed to make package of decoder.

---
+++ archiveDecoder/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+ 
+setup(name='archiveDecoder',
+ 
+      version='0.3',
+ 
+      url='https://github.com/Godis715/Core-Sample-Analysis/tree/master/archiveDecoder',
+ 
+      license='MIT',
+ 
+      author='Dmitry Tereschenko',
+ 
+      author_email='dima123-99@mail.ru',
+  
+      packages= ['archiveDecoder-package'],
+ 
+      long_description=open('README.rst').read(),
+
+      zip_safe=False
+      )
+  
+ 
--- dataAnalysis/app/view.py
+++ dataAnalysis/app/view.py
@@ -3,7 +3,6 @@ from flask import jsonify, abort, make_response, request
 
 from zipfile import ZipFile
 
-
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
