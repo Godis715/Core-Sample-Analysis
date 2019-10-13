b97452afa8c0d4f87c97a95cd8bf88d8b99d8025
Терещенко Дмитрий <dima123-99@mail.ru>
2019-10-13 03:35:44 +0300

    Made edits of upload.

--- backend/backend/apps/core_sample/views.py
+++ backend/backend/apps/core_sample/views.py
@@ -10,37 +10,34 @@ from rest_framework.status import (
 from rest_framework.response import Response
 
 from zipfile import ZipFile
-
 from archiveDecoder import decode_archive
+
+
+def upload_server(data):
+    return 'id'
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] == 'zip'
 
+
 @csrf_exempt
 @api_view(["POST"])
 def upload(request):
-    """Decoding archive -> load data -> result(json)"""
-    """result:
-        {'Type': 'Error', 'Message:': '...'}
-        or
-        {'Type': 'Success', 'Data:': result_analysis} 
-    """
+    """Decoding archive -> load data -> response(json)"""
 
     file = request.FILES['archive']
 
     if allowed_file(file.name):
         zip_file = ZipFile(file, 'r')
         result_decode = decode_archive(zip_file)
-        if result_decode['Type'] == 'Error':
+        if result_decode['Type'] == 'Success':
+            csId = upload_server(result_decode['Data'])
+            return Response({'csId:': csId, 'warnings': result_decode['Warnings']}, status=HTTP_200_OK)
+        elif result_decode['Type'] == 'Error':
             return Response({'message:': result_decode['Message']}, status=HTTP_400_BAD_REQUEST)
         else:
-            ...
-            # csId = upload_server(result_decode)
-            # if result_decode['Type'] == 'Success':
-            #     return Response({'csId:': csId}, status=HTTP_200_OK)
-            # if result_decode['Type'] == 'Warnings':
-            #     return Response({'csId:': csId, 'warnings': result_decode['Warnings']}, status=HTTP_200_OK)
+            raise Exception('Not correct result of decode')
 
     return Response({'Message:': 'Error format file (Expected .zip)'}, status=HTTP_400_BAD_REQUEST)
 
