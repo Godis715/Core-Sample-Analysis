e5e7758bf87801890e25bcaa37831e617c1a5d90
unknown <godis7.15@gmail.com>
2019-10-14 12:04:11 +0300

    Started uploading page

--- frontend/src/components/pages/Account.vue
+++ frontend/src/components/pages/Account.vue
@@ -1,44 +1,19 @@
 <template>
 <div>
     <site-header></site-header>
-    <progressive-cs-viewer
-        :dlImgsLow="dlLow"
-        :uvImgsLow="uvLow"
-        :dlImgsHigh="dlHigh"
-        :uvImgsHigh="uvHigh"
-    ></progressive-cs-viewer>
+    <div><router-link :to="{name:'Upload'}">Upload</router-link></div>
 </div>
 </template>
 
 <script>
     import SiteHeader from '../fragments/Header.vue';
-    import ProgressiveCSViewer from '../fragments/ProgressiveCSViewer.vue';
 
     export default {
         name: 'Account',
-        components: { SiteHeader, 'progressive-cs-viewer': ProgressiveCSViewer },
+        components: { SiteHeader },
         data() {
             return {
-                dlLow: [
-                    { 
-                        src: 'https://vk.com/doc257617715_519686473?hash=fe739cd68b31871ae4&dl=6e1d321a1277722745',
-                        size: { height: '500px', width: '50px' }
-                    },
 
-                    { 
-                        src: '',
-                        size: { height: '', width: '' }
-                    }
-                ],
-                uvLow: [
-
-                ],
-                dlHigh: [
-
-                ],
-                uvHigh: [
-
-                ]
             }
         }
     };
---
+++ frontend/src/components/pages/Upload.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <site-header></site-header>
+    <div><router-link :to="{name:'Account'}">Account</router-link></div>
+</div>
+</template>
+
+<script>
+    import SiteHeader from '../fragments/Header.vue';
+
+    export default {
+        name: 'Upload',
+        components: { SiteHeader },
+        data() {
+            return {
+            }
+        }
+    };
+</script>
