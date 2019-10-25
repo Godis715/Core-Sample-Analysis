eb9a89b8a8c8429ed1c5ee463c8dde4d0ab4dbf7
Терещенко Дмитрий <dima123-99@mail.ru>
2019-10-25 08:40:13 +0300

    Added the changing markup

--- backend/backend/tests/testApi.py
+++ backend/backend/tests/testApi.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 import os
 
 # from Crypto.Cipher import AES
@@ -57,6 +58,19 @@ if token:
     #                          files={"archive": file_obj},
     #                          data={'csName': 'Тест'},
     #                          headers=headers)
+    # print(response.status_code)
+    # print(response.text)
+
+    # csId = '643da111-2c1a-4556-ae60-6d11948e3f1d'
+    # response = requests.get(f"{url}core_sample/{csId}/markup", headers=headers)
+    # print(response.status_code)
+    # print(response.text)
+    #
+    # data = json.loads(response.text)
+    #
+    # response = requests.put(f"{url}core_sample/markup/{csId}", data={'markup': json.dumps(data['markup'])}, headers=headers)
+    # print(response.status_code)
+    # print(response.text)
 
     # response = requests.post(f"{url}logout", headers=headers)
 
@@ -64,8 +78,8 @@ if token:
     # #csId = 4
     # response = requests.delete(f"{url}core_sample/{csId}/delete", headers=headers)
 
-    csId = '63b0ddcf-b28f-430b-add8-ac0b14a5be63'
-    response = requests.get(f"{url}core_sample/{csId}/markup", headers=headers)
+    # csId = '63b0ddcf-b28f-430b-add8-ac0b14a5be63'
+    # response = requests.get(f"{url}core_sample/{csId}/markup", headers=headers)
 
     # response = requests.get(f"{url}core_sample/", headers=headers)
 
@@ -73,16 +87,16 @@ if token:
     # csId = '00e81d72-c8de-4a1e-bf3b-d46a6a6fe93c'
     # response = requests.get(f"{url}core_sample/{csId}", headers=headers)
 
-    # csId = '63b0ddcf-b28f-430b-add8-ac0b14a5be63'
-    # response = requests.put(f"{url}core_sample/{csId}/analyse", headers=headers)
+    csId = '643da111-2c1a-4556-ae60-6d11948e3f1d'
+    response = requests.put(f"{url}core_sample/{csId}/analyse", headers=headers)
 
     # csIds = ['4ebf3a6f-ce3c-4844-b60c-587321b438a0', 'b664248e-09a2-4e2e-8934-dade9cf31946',
     #          '97966069-45b9-4091-960b-72451939f342']
     # response = requests.put(f"{url}core_sample/status", data={'csIds': json.dumps(csIds)}, headers=headers)
     #
 
-    print(response.status_code)
-    print(response.text)
+    # print(response.status_code)
+    # print(response.text)
 else:
     print('No Key')
 
