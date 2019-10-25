eca327ffc4609d24dd7c368a2310537340747d08
Терещенко Дмитрий <dima123-99@mail.ru>
2019-10-25 13:25:49 +0300

    Reversed name of models.

--- backend/backend/apps/core_sample/admin.py
+++ backend/backend/apps/core_sample/admin.py
@@ -1,98 +1,98 @@
 from django.contrib import admin
 
 from .models import \
-    Core_sample_m, \
-    Fragment_m, \
-    Markup_m, \
-    Oil_layer_m, \
-    Rock_layer_m, \
-    Carbon_layer_m, \
-    Ruin_layer_m
+    Core_sample, \
+    Fragment, \
+    Markup, \
+    Oil_layer, \
+    Rock_layer, \
+    Carbon_layer, \
+    Ruin_layer
 
 
 class Core_sample_admin(admin.ModelAdmin):
     """Керн"""
-    list_display = [field.name for field in Core_sample_m._meta.fields]
+    list_display = [field.name for field in Core_sample._meta.fields]
 
     search_fields = ['name']
     list_filter = ['user_id', 'deposit', 'hole', 'status', 'date']
 
     class Meta:
-        model = Core_sample_m
+        model = Core_sample
 
 
 class Fragment_admin(admin.ModelAdmin):
     """Фрагменты керна"""
-    list_display = [field.name for field in Fragment_m._meta.fields]
+    list_display = [field.name for field in Fragment._meta.fields]
 
     list_filter = ['cs_id']
     exclude = ['dl_src', 'uv_src']
 
     class Meta:
-        model = Fragment_m
+        model = Fragment
 
 
 class Markup_admin(admin.ModelAdmin):
     """Разметка керна"""
-    list_display = [field.name for field in Markup_m._meta.fields]
+    list_display = [field.name for field in Markup._meta.fields]
 
     list_filter = ['user_id', 'cs_id', 'date']
     search_fields = ['id']
 
     class Meta:
-        model = Markup_m
+        model = Markup
 
 
 class Oil_layer_admin(admin.ModelAdmin):
     """Слой нефтенасыщенности"""
-    list_display = [field.name for field in Oil_layer_m._meta.fields]
+    list_display = [field.name for field in Oil_layer._meta.fields]
 
     list_filter = ['markup_id']
     search_fields = ['id']
 
     class Meta:
-        model = Oil_layer_m
+        model = Oil_layer
 
 
 class Rock_layer_admin(admin.ModelAdmin):
     """Слой породы"""
-    list_display = [field.name for field in Rock_layer_m._meta.fields]
+    list_display = [field.name for field in Rock_layer._meta.fields]
 
     list_filter = ['markup_id']
     search_fields = ['id']
 
     class Meta:
-        model = Rock_layer_m
+        model = Rock_layer
 
 
 class Carbon_layer_admin(admin.ModelAdmin):
     """Слой карбонатности"""
-    list_display = [field.name for field in Carbon_layer_m._meta.fields]
+    list_display = [field.name for field in Carbon_layer._meta.fields]
 
     list_filter = ['markup_id']
     search_fields = ['id']
 
     class Meta:
-        model = Carbon_layer_m
+        model = Carbon_layer
 
 
 class Disruption_layer_admin(admin.ModelAdmin):
     """Слой разрушенности"""
-    list_display = [field.name for field in Ruin_layer_m._meta.fields]
+    list_display = [field.name for field in Ruin_layer._meta.fields]
 
     list_filter = ['markup_id']
     search_fields = ['id']
 
     class Meta:
-        model = Ruin_layer_m
+        model = Ruin_layer
 
 
-admin.site.register(Core_sample_m, Core_sample_admin)
-admin.site.register(Fragment_m, Fragment_admin)
-admin.site.register(Markup_m, Markup_admin)
-admin.site.register(Oil_layer_m, Oil_layer_admin)
-admin.site.register(Rock_layer_m, Rock_layer_admin)
-admin.site.register(Carbon_layer_m, Carbon_layer_admin)
-admin.site.register(Ruin_layer_m, Disruption_layer_admin)
+admin.site.register(Core_sample, Core_sample_admin)
+admin.site.register(Fragment, Fragment_admin)
+admin.site.register(Markup, Markup_admin)
+admin.site.register(Oil_layer, Oil_layer_admin)
+admin.site.register(Rock_layer, Rock_layer_admin)
+admin.site.register(Carbon_layer, Carbon_layer_admin)
+admin.site.register(Ruin_layer, Disruption_layer_admin)
 
 
