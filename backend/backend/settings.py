b84a97be79d3c1ec083b758ede6537eea5126932
Терещенко Дмитрий <dima123-99@mail.ru>
2019-11-07 21:38:20 +0300

    Deleted jenkins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,6 @@ THIRD_PARTY_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'django_jenkins'
 )
 
 LOCAL_APPS = (
@@ -109,7 +108,7 @@ DATABASES = {
 }
 
 """SqLite3"""
-if 'test' in sys.argv or 'jenkins' in sys.argv:
+if 'test' in sys.argv:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
@@ -195,9 +194,3 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_WHITELIST = (
 #    'http://localhost:8080',
 # )
-
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes'
-    )
