cf3ddd1bd2ac7aa2f6098066979762aebf492296
unknown <godis7.15@gmail.com>
2019-11-24 16:17:55 +0300

    Configuring url and port via env variables

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_URL: JSON.stringify(process.env.VUE_APP_API_URL),
+  API_PORT: JSON.stringify(process.env.VUE_APP_API_PORT)
 })
