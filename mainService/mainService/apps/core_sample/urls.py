0b7a6c81a1435a17fec867959d7d31334ef634f2
Терещенко Дмитрий <dima123-99@mail.ru>
2019-11-26 16:55:00 +0300

    Changed urls

--- mainService/mainService/apps/core_sample/urls.py
+++ mainService/mainService/apps/core_sample/urls.py
@@ -11,18 +11,18 @@ urlpatterns = [
     # GET api/core_sample/
     path('', views.cs_getAll, name="cs_getAll"),
     # GET api/core_sample/<uuid:csId>
-    path('<uuid:csId>', views.cs_get, name="cs_get"),
+    path('<uuid:csId>/', views.cs_get, name="cs_get"),
     # DELETE api/core_sample/<uuid:csId>/delete <-- * '/delete' is the temporary decision *
-    path('<uuid:csId>/delete', views.cs_delete, name="cs_delete"),
+    path('<uuid:csId>/delete/', views.cs_delete, name="cs_delete"),
     # PUT api/core_sample/<uuid:csId>/analyse
-    path('<uuid:csId>/analyse', views.cs_analyse, name="cs_analyse"),
+    path('<uuid:csId>/analyse/', views.cs_analyse, name="cs_analyse"),
     # GET api/core_sample/<uuid:csId>/markup
-    path('<uuid:csId>/markup', views.cs_markup_get, name="cs_markup_get"),
+    path('<uuid:csId>/markup/', views.cs_markup_get, name="cs_markup_get"),
     # PUT api/core_sample/<uuid:csId>/markup/put <-- * '/put' is the temporary decision *
-    path('<uuid:csId>/markup/put', views.cs_markup_put, name="cs_markup_put"),
+    path('<uuid:csId>/markup/put/', views.cs_markup_put, name="cs_markup_put"),
     # POST api/core_sample/upload
-    path('upload', views.cs_upload, name="cs_upload"),
+    path('upload/', views.cs_upload, name="cs_upload"),
     # PUT api/core_sample/status
-    path('status', views.css_status, name="css_status"),
+    path('status/', views.css_status, name="css_status"),
 ]
 
