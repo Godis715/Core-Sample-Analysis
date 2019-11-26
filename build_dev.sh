c7bfc7a7739b65b783578651d976227dd42960d1
Терещенко Дмитрий <dima123-99@mail.ru>
2019-11-26 12:27:41 +0300

    Added dev/prod build of frontend

---
+++ build_dev.sh
@@ -0,0 +1,6 @@
+set VUE_APP_API_URL=http://localhost
+set VUE_APP_API_PORT=8000
+cd frontend
+npm run build
+cd ..
+python build.py
---
+++ build_prod.sh
@@ -0,0 +1,6 @@
+set VUE_APP_API_URL=http://46.149.233.52
+set VUE_APP_API_PORT=8000
+cd frontend
+npm run build
+cd ..
+python build.py
